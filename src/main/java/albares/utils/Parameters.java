102b910a27f97e6ae17a81451ab82a3d2968d030
josem <josem@LAPTOP-S1P10O6I>
2021-10-18 09:00:48 +0200

    Sin logger

--- src/main/java/albares/utils/Parameters.java
+++ src/main/java/albares/utils/Parameters.java
@@ -1,8 +1,6 @@
 
 package albares.utils;
 
-import java.util.logging.Logger;
-
 
 public final class Parameters {
 
@@ -12,5 +10,4 @@ public final class Parameters {
     public static final String PROJECT_NAME = "Jose";
     public static final int EXPIRATION_MILLIS = 60 /* SECONDS */ *1000;
     
-    public static final Logger logger = Logger.getLogger("basicProject.jose");
 }
