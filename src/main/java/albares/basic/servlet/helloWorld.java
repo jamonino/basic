9110bc8d729824d571ed3e56894a6d5455c29528
josem <josem@LAPTOP-S1P10O6I>
2021-10-20 13:42:13 +0200

    form

--- src/main/java/albares/basic/servlet/helloWorld.java
+++ src/main/java/albares/basic/servlet/helloWorld.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/world")
 public class helloWorld extends HttpServlet {
@@ -14,14 +15,17 @@ public class helloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        response.getOutputStream().println("<!DOCTYPE html>");
-        response.getOutputStream().println("<html>");
-        response.getOutputStream().println("<head>");
-        response.getOutputStream().println("<title>Servlet holaMundo</title>");            
-        response.getOutputStream().println("</head>");
-        response.getOutputStream().println("<body>");
-        response.getOutputStream().println("<h1>Servlet holaMundo at " + request.getContextPath() + "</h1>");
-        response.getOutputStream().println("</body>");
-        response.getOutputStream().println("</html>");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet holaMundo</title>");            
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet holaMundo at " + request.getContextPath() + "</h1>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
---
+++ src/main/webapp/createUser.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Create User</title>
+    </head>
+    <body>
+        <form>
+            Hello World!
+        </form>
+    </body>
+</html>
